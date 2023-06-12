4e3652efb31c9bedade975158e4e6da7cebdb13b
NischayYadav615 <112478816+NischayYadav615@users.noreply.github.com>
2023-06-12 11:46:19 +0530

    Add files via upload

---
+++ plugins/Rename/RenameVid.py
@@ -0,0 +1,57 @@
+import time, os
+from pyrogram import Client, filters, enums
+from plugins.helpers.config import DOWNLOAD_LOCATION, CAPTION, ADMINS
+from plugins.helpers.utils import progress_message, humanbytes
+
+@Client.on_message(filters.private & filters.command("vidrename") & filters.user(ADMINS))             
+async def rename_file(bot, msg):
+    reply = msg.reply_to_message
+    if len(msg.command) < 2 or not reply:
+       return await msg.reply_text("Please Reply To An File or video or audio With filename + .extension eg:-(`.mkv` or `.mp4` or `.zip`)")
+    media = reply.document or reply.audio or reply.video
+    if not media:
+       await msg.reply_text("Please Reply To An File or video or audio With filename + .extension eg:-(`.mkv` or `.mp4` or `.zip`)")
+    og_media = getattr(reply, reply.media.value)
+    new_name = msg.text.split(" ", 1)[1]
+    sts = await msg.reply_text("Trying to Downloading.....")
+    c_time = time.time()
+    downloaded = await reply.download(file_name=new_name, progress=progress_message, progress_args=("Download Started.....", sts, c_time)) 
+    filesize = humanbytes(og_media.file_size)                
+    if CAPTION:
+        try:
+            cap = CAPTION.format(file_name=new_name, file_size=filesize)
+        except Exception as e:            
+            return await sts.edit(text=f"Your caption Error unexpected keyword ●> ({e})")           
+    else:
+        cap = f"{new_name}\n\n💽 size : {filesize} \n\nᴊᴏɪɴ : @bigmoviesworld \nᴍᴏᴠɪᴇs : @NasraniSeries"
+    # this idea's back end is MKN brain 🧠
+
+    dir = os.listdir(DOWNLOAD_LOCATION)
+    if len(dir) == 0:
+        file_thumb = await bot.download_media(og_media.thumbs[0].file_id)
+        og_thumbnail = file_thumb
+    else:
+        try:
+            og_thumbnail = f"{DOWNLOAD_LOCATION}/thumbnail.jpg"
+        except Exception as e:
+            print(e)        
+            og_thumbnail = None
+        
+    await sts.edit("Trying to Uploading")
+    c_time = time.time()
+    try:
+        await bot.send_video(msg.chat.id, video=downloaded, thumb=og_thumbnail, caption=cap, progress=progress_message, progress_args=("Uploade Started.....", sts, c_time))        
+    except Exception as e:  
+        return await sts.edit(f"Error {e}")                       
+    try:
+        if file_thumb:
+            os.remove(file_thumb)
+        os.remove(downloaded)      
+    except:
+        pass
+    await sts.delete()
+
+
+
+
+
